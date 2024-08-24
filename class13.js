5afb74e9ebfc17358e734577a525289b1b68565d
mdmojaffor98@gmail.com <mdmojaffor98@gmail.com>
2024-08-24 21:58:04 +0600

    Relational Operator And Logical Operator

---
+++ class13.js
@@ -0,0 +1,22 @@
+
+// Relational Operator
+var num1= 30;
+var num2 ="30";
+
+console.log(num2>num1);false
+console.log(num2<num1); true 
+console.log(num1<=num2); 
+console.log(num1==num2); 
+console.log(num1==num2); //just value check 
+console.log(num1===num2);// value with data type check 
+
+// Logical Operatior
+
+var num1= 40; 
+var num2 =30;
+var num3 =20;
+
+console.log(num1>num2 && num2<num3); false 
+console.log(num1>num2 && num2>num3); true 
+console.log(num1>num2 || num2<num3); true 
+console.log(num1<num2 || num2<num3); false 
