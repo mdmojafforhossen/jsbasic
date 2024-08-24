dc617b8e4ed9d2f0ac41900103b41c497a0668f0
mdmojaffor98@gmail.com <mdmojaffor98@gmail.com>
2024-08-25 00:32:17 +0600

    switch case class complete

---
+++ class17.js
@@ -0,0 +1,27 @@
+
+var letter = prompt("Enter a Letter :")
+var letter =letter.toLowerCase();
+switch(letter){
+    case "a":
+    console.log("Vowel")
+    break;
+
+    case "e":
+    console.log("Vowel")
+    break;
+
+    case "i":
+    console.log("Vowel")
+    break;
+
+    case "o":
+    console.log("Vowel")
+    break;
+
+    case "u":
+    console.log("Vowel")
+    break;
+
+    default:
+        console.log("Consonant");
+}
