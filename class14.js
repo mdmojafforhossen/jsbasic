53abc90c57ad51df9249732f4ea35e137ac67589
mdmojaffor98@gmail.com <mdmojaffor98@gmail.com>
2024-08-24 22:45:59 +0600

    Control Statement if,else if

---
+++ class14.js
@@ -0,0 +1,19 @@
+
+var num = prompt("Enter your Number:");
+
+
+if(num>=80)
+    console.log("A+");
+else if(num>=70)
+    console.log("A");
+else if(num>=60)
+    console.log("A-");
+else if(num>=50)
+    console.log("B+");
+else if(num>=40)
+    console.log("B");
+else if(num>=33)
+    console.log("c");
+else
+console.log("Fail");
+    
