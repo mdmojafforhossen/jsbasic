14ea4e579a81f16b3362fcd58349355d8985f3a6
mdmojaffor98@gmail.com <mdmojaffor98@gmail.com>
2024-08-25 20:57:59 +0600

    traditional function

---
+++ class24.js
@@ -0,0 +1,31 @@
+
+
+function sum(num1,num2){
+    var result =num1+num2
+    document.write("Result = "+result+"</br>");
+}
+sum(30,20);
+
+function sub(num1,num2){
+    var result =num1-num2
+    document.write("Result = "+result+"</br>");
+}
+sub(30,20);
+
+function gun(num1,num2){
+    var result =num1*num2
+    document.write("Result = "+result+"</br>");
+}
+gun(30,20);
+
+function vag(num1,num2){
+    var result =num1/num2
+    document.write("Result = "+result+"</br>");
+}
+vag(30,20);
+
+function vagsehs(num1,num2){
+    var result =num1%num2
+    document.write("Result = "+result+"</br>");
+}
+vagsehs(30,20);
