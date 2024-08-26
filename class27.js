08a61288b0e7d7db66ebf73ea2cf034e485872fc
mdmojaffor98@gmail.com <mdmojaffor98@gmail.com>
2024-08-26 11:31:39 +0600

    How to loop in array

---
+++ class27.js
@@ -0,0 +1,11 @@
+
+
+
+var num = [20, 40, 30, 50, 60];
+var sum=0;
+
+for(var x=0; x<5; x++){
+    document.write( " "+num [x]);
+    var sum=sum+num[x];
+}
+document.write("</br>Total Resutl="+sum)
