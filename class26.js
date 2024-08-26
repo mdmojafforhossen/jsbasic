539024f5cc18a292c46f5d894c732b2eb15b81e5
mdmojaffor98@gmail.com <mdmojaffor98@gmail.com>
2024-08-26 11:33:33 +0600

    how to creat an array

---
+++ class26.js
@@ -0,0 +1,15 @@
+
+
+var country1 = ["Bangladesh","India","Italy"];
+var country2 = ["Vutan","Maldiv","Srilanka"];
+
+country1.push("Saudi"); //Add Element last
+country1.pop(); //Remove Element last
+
+// var addcountry = country1.concat(country2);
+
+// document.write(addcountry);
+
+// document.write(addcountry.length);
+
+
