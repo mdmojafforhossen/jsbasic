e3b618ee57b82e1623da59c2ec9a86a11916543f
mdmojaffor98@gmail.com <mdmojaffor98@gmail.com>
2024-08-28 22:26:18 +0600

    modify some line

--- class40.js
+++ class40.js
@@ -1,14 +1,23 @@
 
 
 
+// function addst(){
+//     document.querySelector("#para").innerHTML="CSS Style Add";
+//     document.querySelector("#para").style.color="red";
+//     document.querySelector("#para").style.fontSize="30px";
+// }
+// function remov(){
+//     document.querySelector("#para").innerHTML="CSS Style Remove";
+//     document.querySelector("#para").style.color="black";
+//     document.querySelector("#para").style.fontSize="16px";
+
+// }
+
 function addst(){
-    document.querySelector("#para").innerHTML="CSS Style Add";
-    document.querySelector("#para").style.color="red";
-    document.querySelector("#para").style.fontSize="30px";
+    var addstle= document.getElementById("para");
+    addstle.classList.add("para-stle");
 }
 function remov(){
-    document.querySelector("#para").innerHTML="CSS Style Remove";
-    document.querySelector("#para").style.color="black";
-    document.querySelector("#para").style.fontSize="16px";
-
+    var remove= document.querySelector("#para");
+    remove.classList.remove("para-stle");
 }
--- class9.js
+++ class9.js
@@ -1,5 +1,5 @@
-var num1 = prompt("Enter your first Number:");
-var num2 = prompt("Enter your Last Number:");
+// var num1 = prompt("Enter your first Number:");
+// var num2 = prompt("Enter your Last Number:");
 
 var num1 = parseFloat(num1,10);
 var num2 = parseFloat(num2,10);
