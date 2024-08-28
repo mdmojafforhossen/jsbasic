cc2a864596725001b312a3e78fc89cbd75d64d39
mdmojaffor98@gmail.com <mdmojaffor98@gmail.com>
2024-08-28 22:16:19 +0600

    css style changing

---
+++ class40.js
@@ -0,0 +1,14 @@
+
+
+
+function addst(){
+    document.querySelector("#para").innerHTML="CSS Style Add";
+    document.querySelector("#para").style.color="red";
+    document.querySelector("#para").style.fontSize="30px";
+}
+function remov(){
+    document.querySelector("#para").innerHTML="CSS Style Remove";
+    document.querySelector("#para").style.color="black";
+    document.querySelector("#para").style.fontSize="16px";
+
+}
