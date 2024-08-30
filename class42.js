7c6415945260e5ab5d25c645d0ab44e4383d4187
mdmojaffor98@gmail.com <mdmojaffor98@gmail.com>
2024-08-30 16:27:08 +0600

    some file added

--- class42.js
+++ class42.js
@@ -1,12 +1,65 @@
 
 
+// var len = document.querySelectorAll(".myButton").length;
 
-for(var i=0; i<3;i++){
-    document.querySelectorAll("#myButton")[i].addEventListener('click',function(){
+// for(var i=0; i<len; i++){
+//     document.querySelectorAll(".myButton")[i].addEventListener('click',function(){
+//         var text = this.innerHTML;
+//         document.querySelector("h1").innerHTML= text+" is clicked";
+//     });
+// }
+
+var len = document.querySelectorAll(".myButton").length;
+
+for(var i=0; i<len; i++){
+    document.querySelectorAll(".myButton")[i].addEventListener('click',function(){
         var text = this.innerHTML;
-        document.querySelector("h1").innerHTML= text+" is clicked";
+        audioPlay(text);
+        playanimat(text);
+       
     });
 }
 
+function audioPlay(text){
+    switch(text){
+
+        case "a":
+            var audio = new Audio('audio/1.m4a');
+            audio.play();
+            break;
+
+            case "b":
+            var audio = new Audio('audio/2.m4a');
+            audio.play();
+            break;
+
+            case "c":
+            var audio = new Audio('audio/3.m4a');
+            audio.play();
+            break;
+
+            case "d":
+            var audio = new Audio('audio/4.m4a');
+            audio.play();
+            break;
+
+            case "e":
+            var audio = new Audio('audio/5.m4a');
+            audio.play();
+            break;
+
+           
+
+    }
+}
 
 
+function playanimat(text){
+    var selectedButton = document.querySelector("."+ text);
+    selectedButton.classList.add("animat");
+
+    setTimeout(function(){
+    selectedButton.classList.remove("animat");
+
+    },1000);
+}
